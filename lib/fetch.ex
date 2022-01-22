9b4bed854cf158a6c67b1c10d105a9165385dba7
ghalestrilo <ghalestrilo@gmail.com>
2022-01-22 09:25:07 -0300

    :white_check_mark: outline tests

--- lib/fetch.ex
+++ lib/fetch.ex
@@ -1,18 +1,38 @@
 defmodule Fetch do
   @moduledoc """
-  Documentation for `Fetch`.
+  This module implements methods to retrieve pages from the web
+  and query their HTML content
   """
 
   @doc """
-  Hello world.
-
   ## Examples
-
-      iex> Fetch.hello()
-      :world
-
+      iex> Fetch.fetch("http://zen.github.io")
+      {
+        :ok,
+        %{
+        assets: [],
+        links: [
+          # "https://githubstatus.com",
+          # "https://twitter.com/githubstatus",
+          # "/",
+          # "/"
+        ]
+      }
+      }
   """
-  def hello do
-    :world
+
+  @spec fetch(url :: String.t()) :: {atom, %{assets: [String.t()], links: [String.t()]}}
+  def fetch(_url) do
+    {:ok, %{assets: [], links: []}}
+  end
+
+  @spec extract_tags(data :: String.t(), tags :: [atom]) :: {atom, [any]}
+  def extract_tags(_data, _tags) do
+    {:ok, []}
+  end
+
+  @spec get_body(url :: String.t()) :: {atom, String.t()}
+  def get_body(_url) do
+    {:ok, ""}
   end
 end
--- test/fetch_test.exs
+++ test/fetch_test.exs
@@ -2,7 +2,25 @@ defmodule FetchTest do
   use ExUnit.Case
   doctest Fetch
 
-  test "greets the world" do
-    assert Fetch.hello() == :world
+  setup %{} do
+    :ok
+  end
+
+  describe "get_body/2" do
+    test "passes" do
+      :ok
+    end
+  end
+
+  describe "extract_tags/2" do
+    test "passes" do
+      :ok
+    end
+  end
+
+  describe "fetch/1" do
+    test "passes" do
+      :ok
+    end
   end
 end
